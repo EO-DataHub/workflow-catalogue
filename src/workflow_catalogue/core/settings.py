9ddde5e8727454e5d97b4d0406be0313aa429985
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2025-11-25 16:43:59 +0100

    feat(core): Add env vars and settings

--- src/workflow_catalogue/core/settings.py
+++ src/workflow_catalogue/core/settings.py
@@ -15,15 +15,93 @@ Examples:
 
 from __future__ import annotations
 
+from urllib.parse import urljoin
+
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from workflow_catalogue import consts
+
+
+class OAuth2Settings(BaseModel):
+    """OAuth2 settings."""
+
+    base_url: str
+    realm: str
+    username: str
+    password: str
+    client_id: str
+
+    @property
+    def oid_url(self) -> str:
+        """Returns the OpenID URL."""
+        return urljoin(self.base_url, f"/keycloak/realms/{self.realm}/protocol/openid-connect")
+
+    @property
+    def token_url(self) -> str:
+        """Returns the token URL."""
+        return self.oid_url + "/token"
+
+    @property
+    def auth_url(self) -> str:
+        """Returns the auth URL."""
+        return self.oid_url + "/auth"
+
+    @property
+    def introspect_url(self) -> str:
+        """Returns the introspect URL."""
+        return self.token_url + "/introspect"
+
+    @property
+    def certs_url(self) -> str:
+        """Returns the certs URL."""
+        return self.oid_url + "/certs"
+
+
+class EODHSettings(OAuth2Settings):
+    """EO Data Hub settings."""
+
+    ades_endpoint_path: str
+    stac_api_endpoint_path: str
+    workspace_services_endpoint_path: str
+    tmp_s3_credentials_endpoint_path: str
+
+    @property
+    def workspace_tokens_url(self) -> str:
+        """Returns the URL for retrieving workspace tokens."""
+        return urljoin(self.base_url, f"{self.workspace_services_endpoint_path}/{self.username}/me/tokens")
+
+    @property
+    def workspace_session_tokens_url(self) -> str:
+        """Returns the URL for retrieving workspace session tokens."""
+        return urljoin(self.base_url, f"{self.workspace_services_endpoint_path}/{self.username}/me/sessions")
+
+    @property
+    def ades_url(self) -> str:
+        """Returns the ADES URL."""
+        return urljoin(self.base_url, self.ades_endpoint_path)
+
+    @property
+    def tmp_s3_credentials_url(self) -> str:
+        """Returns the URL for retrieving temporary S3 credentials."""
+        return urljoin(self.base_url, self.tmp_s3_credentials_endpoint_path)
+
+    @property
+    def stac_url(self) -> str:
+        """Returns the STAC URL."""
+        return urljoin(self.base_url, self.stac_api_endpoint_path)
+
+    @property
+    def workspace_services_url(self) -> str:
+        """Returns the Workspace Services URL."""
+        return urljoin(self.base_url, self.workspace_services_endpoint_path)
 
 
 class Settings(BaseSettings):
     """Represents Application Settings with nested configuration sections."""
 
     environment: str = "local"
+    eodh: EODHSettings
 
     model_config = SettingsConfigDict(
         env_file=consts.directories.ROOT_DIR / ".env",
--- tests/unit/core/test_settings.py
+++ tests/unit/core/test_settings.py
@@ -2,14 +2,59 @@ from __future__ import annotations
 
 from unittest.mock import patch
 
-from workflow_catalogue.core.settings import Settings, current_settings
+from workflow_catalogue.core.settings import current_settings
 
 
+@patch.dict(
+    "os.environ",
+    {
+        "ENVIRONMENT": "test",
+        "EODH__BASE_URL": "https://test.eodatahub.org.uk/",
+        "EODH__REALM": "eodh",
+        "EODH__USERNAME": "test_username",
+        "EODH__PASSWORD": "test_password",
+        "EODH__CLIENT_ID": "test_client_id",
+        "EODH__WORKSPACE_SERVICES_ENDPOINT_PATH": "/workspace-services/v1",
+        "EODH__STAC_API_ENDPOINT_PATH": "/stac/v1",
+        "EODH__ADES_ENDPOINT_PATH": "/ades/v1",
+        "EODH__TMP_S3_CREDENTIALS_ENDPOINT_PATH": "/s3/credentials/v1",
+    },
+)
 def test_settings() -> None:
-    assert current_settings is not None
+    settings = current_settings()
+    assert settings is not None
+    assert settings.environment == "test"
 
+    assert settings.eodh.base_url == "https://test.eodatahub.org.uk/"
+    assert settings.eodh.workspace_services_endpoint_path == "/workspace-services/v1"
+    assert settings.eodh.stac_api_endpoint_path == "/stac/v1"
+    assert settings.eodh.ades_endpoint_path == "/ades/v1"
+    assert settings.eodh.tmp_s3_credentials_endpoint_path == "/s3/credentials/v1"
 
-@patch.dict("os.environ", {"ENVIRONMENT": "production"})
-def test_environment_variable_override() -> None:
-    settings = Settings()
-    assert settings.environment == "production"
+    assert (
+        settings.eodh.workspace_tokens_url
+        == "https://test.eodatahub.org.uk/workspace-services/v1/test_username/me/tokens"
+    )
+    assert (
+        settings.eodh.workspace_session_tokens_url
+        == "https://test.eodatahub.org.uk/workspace-services/v1/test_username/me/sessions"
+    )
+    assert settings.eodh.workspace_services_url == "https://test.eodatahub.org.uk/workspace-services/v1"
+    assert settings.eodh.stac_url == "https://test.eodatahub.org.uk/stac/v1"
+    assert settings.eodh.ades_url == "https://test.eodatahub.org.uk/ades/v1"
+    assert settings.eodh.tmp_s3_credentials_url == "https://test.eodatahub.org.uk/s3/credentials/v1"
+
+    assert settings.eodh.realm == "eodh"
+    assert settings.eodh.username == "test_username"
+    assert settings.eodh.password == "test_password"  # noqa: S105
+    assert settings.eodh.client_id == "test_client_id"
+    assert (
+        settings.eodh.token_url == "https://test.eodatahub.org.uk/keycloak/realms/eodh/protocol/openid-connect/token"  # noqa: S105
+    )
+    assert settings.eodh.auth_url == "https://test.eodatahub.org.uk/keycloak/realms/eodh/protocol/openid-connect/auth"
+    assert (
+        settings.eodh.introspect_url
+        == "https://test.eodatahub.org.uk/keycloak/realms/eodh/protocol/openid-connect/token/introspect"
+    )
+    assert settings.eodh.certs_url == "https://test.eodatahub.org.uk/keycloak/realms/eodh/protocol/openid-connect/certs"
+    assert settings.eodh.oid_url == "https://test.eodatahub.org.uk/keycloak/realms/eodh/protocol/openid-connect"
